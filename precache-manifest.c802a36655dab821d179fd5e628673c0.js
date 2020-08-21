e3928d32bcfcd542a510bec5ea539e9754ef48c7
Charles Uko <charlesuko@Charless-MBP.lan>
2020-08-20 22:22:22 -0400

    Updates

---
+++ precache-manifest.c802a36655dab821d179fd5e628673c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb899f4c2ebba08b51cf1de0b92581b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1f3102b35c803443c644",
+    "url": "/monsters-rolodex/static/css/main.ab7f23ad.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f3102b35c803443c644",
+    "url": "/monsters-rolodex/static/js/main.65237a17.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
